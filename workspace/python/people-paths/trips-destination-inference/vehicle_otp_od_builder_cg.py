2cde817a0fcaf205457d4f3036cdd53e6dc2e974
Veruska Santos <veruska.santos@ccc.ufcg.edu.br>
2019-08-15 11:32:49 -0300

    Updating TODO

--- workspace/python/people-paths/trips-destination-inference/vehicle_otp_od_builder_cg.py
+++ workspace/python/people-paths/trips-destination-inference/vehicle_otp_od_builder_cg.py
@@ -365,7 +365,7 @@ try:
 	
 	
 	output_bulma_otp.to_csv("data/output/output_bulma_otp.csv",index=False)
-    # Garantir que o segundo ônibus só passa depois do desembarque do primeiro
+    # Garantir que o segundo onibus so passa depois do desembarque do primeiro
     # Verificar horarios do segundo onibus, estao com muitas horas de atraso (GTFS?)
     # Verificar se a troca do primeiro onibus pro segundo onibus eh menor que 70 minutos
 
